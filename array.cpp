9119bf7ff03598407a2f1d08dc360795d01d97cc
omniaura <omniaura@cl0.beta.service.aws.koding.com>
2013-05-08 13:44:04 -0700

    findFirstSequence modifications

--- array.cpp
+++ array.cpp
@@ -27,25 +27,26 @@ int findFirst (const string a[], int n, string target) {
 }
 
 int findFirstSequence (const string a[], int n, string target, int &begin, int &end) {
-    bool active = true;
     int length = 0;
     for (int i = 0; i < n; i++) {
-        if (active && a[i] == target) {
+        if (a[i] == target) {
             begin = i;
-            length++;
             for (int j = i+1; j < n; j++) {
                 if (a[j] == target) {
                     length++;
                 }
                 else {
-                    end = begin+(length-1); //was returning 1 too big; fixed by -1?
-                    active = false;
+                    end = begin+length;
                     return true;
                 }
             }
         }
     }
     return false;
+}
+
+int positionOfMin(const string a[], int n) {
+    
 }
 
 int moveToEnd (string a[], int n, int pos) {
@@ -79,9 +80,10 @@ int divide(string a[], int n, string divider) {
 int main() {
     int b = 0;
     int e = 1;
-    string test_array[]  = {"Dolor", "lorem", "lorem", "ipsum", "dolor", "dolor", "sit", "amet", "dolor"};
-    cout << findFirstSequence(test_array, 9, "lorem", b, e);
-    cout << b << " " << e << "\n";
+    string test_array[]  = {"lorem", "lorem", "lorem", "ipsum", "dolor", "dolor", "sit", "lorem", "lorem"};
+    cout << findFirstSequence(test_array, 9, "Lorem", b, e) << "\n";
+    cout << b << ", " << e;
+    
     cout << "\n";
 }
 
