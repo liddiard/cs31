5cbf6837978877d47e10f261dfd6a8176220d02f
omniaura <omniaura@cl0.beta.service.aws.koding.com>
2013-05-09 12:45:24 -0700

    added test routine, fixed errors

--- array.cpp
+++ array.cpp
@@ -4,21 +4,22 @@
 using namespace std;
  
 int tally (const string a[], int n, string target) {
+    if (n<0) {
+        return -1;
+    }
     int tally = 0;
     for (int i = 0; i < n; i++) {
         if (a[i] == target) {
             tally++;
         }
     }
-    if (tally > 0) {
-        return tally;
-    }
-    else {
-        return -1;
-    }
+    return tally;
 }
  
 int findFirst (const string a[], int n, string target) {
+    if (n<0) {
+        return -1;
+    }
     for (int i = 0; i < n; i++) {
         if (a[i] == target) {
             return i;
@@ -27,7 +28,10 @@ int findFirst (const string a[], int n, string target) {
     return -1;
 }
  
-int findFirstSequence (const string a[], int n, string target, int &begin, int &end) {
+bool findFirstSequence (const string a[], int n, string target, int &begin, int &end) {
+    if (n < 0 || begin < 0 || end < 0) {
+        return -1;
+    }
     int length = 0;
     for (int i = 0; i < n; i++) {
         if (a[i] == target) {
@@ -47,7 +51,7 @@ int findFirstSequence (const string a[], int n, string target, int &begin, int &
 }
  
 int positionOfMin(const string a[], int n) {
-    if (n == 0) {
+    if (n < 0) {
         return -1;
     }
     string smallest = a[0];
@@ -62,15 +66,21 @@ int positionOfMin(const string a[], int n) {
 }
  
 int moveToEnd (string a[], int n, int pos) {
-   string pop = a[pos];
-   for (int i = pos; i < (n-1); i++) {
-       a[i] = a[i+1];
-   }
-   a[n-1] = pop;
-   return pos;
+    if (n < 0 || pos < 0) {
+        return -1;
+    }
+    string pop = a[pos];
+    for (int i = pos; i < (n-1); i++) {
+        a[i] = a[i+1];
+    }
+    a[n-1] = pop;
+    return pos;
 }
  
 int moveToBeginning(string a[], int n, int pos) {
+    if (n < 0 || pos < 0) {
+        return -1;
+    }
     string pop = a[pos];
     for (int i = pos; i > 0; i--) {
         a[i] = a[i-1];
@@ -80,7 +90,10 @@ int moveToBeginning(string a[], int n, int pos) {
 }
  
 int disagree(const string a1[], int n1, const string a2[], int n2) {
-    int match;
+    if (n1 < 0 || n2 < 0) {
+        return -1;
+    }
+    int match = -1;
     for (int i = 0; i < n1; i++) {
         match = i;
         if (a1[i] != a2[i]) {
@@ -96,6 +109,9 @@ int disagree(const string a1[], int n1, const string a2[], int n2) {
 }
  
 int removeDups(string a[], int n) {
+    if (n < 0) {
+        return -1;
+    }
     int removed = 0;
     int traverse = n;
    for (int i = 0; i < traverse; i++) {
@@ -112,25 +128,30 @@ int removeDups(string a[], int n) {
            else {;}
        }
    }
-   return removed;
+   return n-removed;
 }
  
 bool subsequence(const string a1[], int n1, const string a2[], int n2) {
+    if (n1 < 0 || n2 < 0) {
+        return -1;
+    }
    int substring_pos = 0;
+   bool all_found = false;
    for (int i = 0; i < n1; i++) {
-       if (a1[i] == a2[substring_pos]) {
+       if (a1[i] == a2[substring_pos] && substring_pos < (n2-1)) {
            substring_pos++;
        }
+       else if (a1[i] == a2[substring_pos] && substring_pos == (n2-1)) {
+           all_found = true;
+       } else {}
    }
-   if (substring_pos >= n2) { //technically bad coding; should be "=="
-       return true;
-   }
-   else {
-       return false;
-   }
+    return all_found;
 }
  
 int mingle(const string a1[], int n1, const string a2[], int n2, string result[], int max) {
+    if (n1 < 0 || n2 < 0 || max < 0) {
+        return -1;
+    }
     for (int i = 1; i < n1; i++) {
         if (a1[i] < a1[i-1]) {
             return -1;
@@ -164,6 +185,9 @@ int mingle(const string a1[], int n1, const string a2[], int n2, string result[]
 }
  
 int divide(string a[], int n, string divider) {
+    if (n < 0) {
+        return -1;
+    }
     for (int i = 0; i < n; i++) {
         moveToEnd(a, n, positionOfMin(a, n-i)); //tricky, huh? decreases the scope the function's looking at with each iteration. ends up sorting everything alphabetically.
     }
@@ -172,17 +196,39 @@ int divide(string a[], int n, string divider) {
             return i;
         }
     }
+    return -1; //error with the function if we reach here
 }
  
 int main() {
-    int b = 0;
-    int e = 1;
-    string test_array[]  = {"beads", "bees", "buster", "sit", "zeta", "zzyzx", "zzyzx"};
-    string test_array_2[]  = {"amet", "dolor", "lorem"};
-    cout << divide(test_array, 7, "lucille") << "\n";
-    for (int i = 0; i < 7; i++) {
-        cout << test_array[i];
-    }
+        string h[7] = { "peter", "lois", "meg", "chris", "", "stewie", "meg" };
+	    assert(tally(h, 7, "meg") == 2);
+	    assert(tally(h, 7, "") == 1);
+	    assert(tally(h, 7, "quagmire") == 0);
+	    assert(tally(h, 0, "meg") == 0);
+	    assert(findFirst(h, 7, "meg") == 2);
+	    assert(findFirst(h, 2, "meg") == -1);
+	    int bg;
+	    int en;
+	    assert(findFirstSequence(h, 7, "chris", bg, en) && bg == 3 && en == 3);
 
-    cout << "\n";
+	    string g[4] = { "peter", "lois", "chris", "stewie" };
+	    assert(positionOfMin(g, 4) == 2);
+	    assert(disagree(h, 4, g, 4) == 2);
+	    assert(subsequence(h, 7, g, 4));
+	    assert(moveToEnd(g, 4, 1) == 1 && g[1] == "chris" && g[3] == "lois");
+	
+	    string f[4] = { "chris", "stewie", "meg", "lois" };
+	    assert(moveToBeginning(f, 4, 2) == 2 && f[0] == "meg" && f[2] == "stewie");
+	
+	    string e[5] = { "chris", "chris", "chris", "meg", "meg" };
+	    assert(removeDups(e, 5) == 2 && e[1] == "meg");
+	
+	    string x[4] = { "brian", "lois", "lois", "quagmire" };
+	    string y[4] = { "chris", "lois", "meg", "stewie" };
+	    string z[10];
+	    assert(mingle(x, 4, y, 4, z, 10) == 8 && z[5] == "meg");
+	
+	    assert(divide(h, 7, "meg") == 3);
+	
+	    cout << "All tests succeeded" << endl;
 }
