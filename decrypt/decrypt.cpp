df4537de6d1396535d613c89f1850afa8b53ef22
omniaura <omniaura5@gmail.com>
2013-05-13 13:30:44 -0700

    added main, decrypt function

---
+++ decrypt/decrypt.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+using namespace std;
+
+bool decrypt(istream& cipherstream, const char crib[]) {
+
+}
+
+int main () {
+    ifstream infile("test_input.txt");
+}
