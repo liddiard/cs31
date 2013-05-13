9ded2186aec76cfe4c6a0d94fd99f2fba745e2a7
omniaura <omniaura5@gmail.com>
2013-05-13 13:51:47 -0700

    added file reading to main

--- decrypt/decrypt.cpp
+++ decrypt/decrypt.cpp
@@ -9,4 +9,13 @@ bool decrypt(istream& cipherstream, const char crib[]) {
 
 int main () {
     ifstream infile("test_input.txt");
+    if (!infile) {
+        cerr << "opening file failed.";
+    } else {
+        cout << "opening file successful.";
+    }
+    const int MAX = 200;
+	char line[MAX];
+	infile.getline(line, MAX);
+    cout << line;
 }
