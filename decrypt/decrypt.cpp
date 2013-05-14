ee931e41066822b81da8f34eb34fad7825986434
omniaura <omniaura5@gmail.com>
2013-05-14 11:07:19 -0700

    added toUpperCase, toLowerCase functions

--- decrypt/decrypt.cpp
+++ decrypt/decrypt.cpp
@@ -1,21 +1,41 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
+#include <cctype>
 using namespace std;
 
-bool decrypt(istream& cipherstream, const char crib[]) {
+void makeLowerCase(char s[]) {
+    for (int k=0; s[k] != '\0'; k++) {
+        s[k] = tolower(s[k]);
+    }
+}
 
+void makeUpperCase(char s[]) {
+    for (int k=0; s[k] != '\0'; k++) {
+        s[k] = toupper(s[k]);
+    }
+}
+
+bool decrypt(istream& cipherstream, const char crib[]) {
+    
 }
 
 int main () {
     ifstream infile("test_input.txt");
     if (!infile) {
         cerr << "opening file failed.";
-    } else {
-        cout << "opening file successful.";
     }
-    const int MAX = 200;
-	char line[MAX];
-	infile.getline(line, MAX);
-    cout << line;
+    const int MAXCOL = 80 +1;
+    const int MAXROW = 50;
+    char line[MAXCOL];
+    char input_file[MAXROW][MAXCOL];
+    
+    int line_number = 0;
+    while (line[0] != '\0') {
+        infile.getline(line, MAXCOL);
+        strcpy(line, input_file[line_number]);
+        //cout << line << "\n";
+        line_number++;
+    }
+    cout << input_file[1];
 }
