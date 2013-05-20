10aa0e121be3d22aac7262d2294d34fb43f5eb9b
omniaura <omniaura5@gmail.com>
2013-05-19 23:18:40 -0700

    checking if key is valid or not

--- decrypt/decrypt.cpp
+++ decrypt/decrypt.cpp
@@ -91,12 +91,11 @@ bool decrypt(istream& cipherstream, char crib[]) {
         }
     }
     
-    
     /* use length-comparing heuristic */
-    int match_beginning_point[MAXCHAR];
-    int match_beginning_point_head = 0;
+    int match_beginning_word[MAXCHAR];
+    int match_beginning_word_head = 0;
     for (int i = 0; i < MAXCHAR; i++) {
-        match_beginning_point[i] = '\0'; //visit every character in the array and set it to the null byte
+        match_beginning_word[i] = '\0'; //visit every character in the array and set it to the null byte
     }
     for (int i = 0; i <= word_number; i++) {
         if (strlen(separate[i]) == strlen(crib_separate[0])) {
@@ -104,16 +103,58 @@ bool decrypt(istream& cipherstream, char crib[]) {
                 if (strlen(separate[i+j]) != strlen(crib_separate[j])) {
                     break;
                 } else if (j == crib_word_number) {
-                    match_beginning_point[match_beginning_point_head] = i;
-                    match_beginning_point_head++;
+                    match_beginning_word[match_beginning_word_head] = i;
+                    match_beginning_word_head++;
                 }
             }
         }
     }
     
     /* test output */
-    for (int i = 0; i < match_beginning_point_head; i++) {
-        cout << match_beginning_point[i] << ", ";
+    for (int i = 0; i < match_beginning_word_head; i++) {
+        cout << match_beginning_word[i] << "\n";
+    }
+    
+    /* match_beginning_word is an array of integers that stores
+     * the positions of the first WORD in each phrase which fully matches the crib's length heuristic */
+    
+    /* construct a simple letter-to-letter key */
+    char key[MAXCHAR/2][2];
+    for (int i = 0; i < match_beginning_word_head; i++) { //for each match
+        int key_head = 0;
+        cout << "***" << i << "***" << "\n"; //test output
+        for (int j = 0; j <= crib_word_number; j++) { //for each word in each match
+            for (int k = 0; k < strlen(crib_separate[j]); k++) { //for each character in each word
+                key[key_head][0] = separate[(match_beginning_word[i]+j)][k];
+                key[key_head][1] = crib_separate[j][k];
+                key_head++;
+            }
+        }
+        
+        /* test output */
+        for (int j = 0; j < key_head; j++) {
+            cout << key[j][0] << " : " << key[j][1] << "\n";
+        }
+        
+        /* check if the key is valid (has unique letter assignments) */
+        int message_match = 0;
+        int crib_match = 0;
+        for (int j = 0; j < key_head; j++) { //for each letter...
+            for (int k = 0; k < key_head; k++) { //...compare to each other letter
+                if (key[j][0] == key[k][0]) {
+                    message_match++;
+                    if (key[j][1] != key[k][1]) {
+                        cout << "invalid key";
+                        goto checkMatch; //ruh-roh, they don't match, this key is not valid
+                    }
+                    crib_match++;
+                }
+            }
+        }
+        if (crib_match == message_match) {
+            cout << "valid key found!"; //for each letter that has a match, cout the uppercase version, otherwise cout the original w/o transforming case, and return true
+        }
+        checkMatch: ; //placed here so i auto-incremented
     }
     
 }
@@ -126,7 +167,7 @@ int main () {
         return -1;
     }
     
-    char test_crib[10] = "Hello son";
+    char test_crib[10] = "Heleo son";
     
     decrypt(infile, test_crib);
     
