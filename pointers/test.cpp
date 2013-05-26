80e89cd35ba95da54cd3c92ee9ea6bb2d8ef2b98
omniaura <omniaura5@gmail.com>
2013-05-25 22:32:10 -0700

    fixed bool b assignment in question 2

--- pointers/test.cpp
+++ pointers/test.cpp
@@ -1,23 +1,19 @@
 #include <iostream>
 using namespace std;
 
-void deleteG(char s[]) {
-    char* ptr = s;
-    while (*ptr != '\0') {
-        if (*ptr == 'g' || *ptr == 'G') {
-            while(*ptr != '\0') {
-                *ptr = *(ptr+1); //move everything forward one to delete the offending 'g'/'G'
-                ptr++;
-            }
-            ptr = s; //move the pointer back to the beginning of the array (somewhat inefficient, but I'm not a genius yet okay Smallberg)
-        }
-        ptr++;
-    }
-}
-
-int main()
-{
-    char msg[100] = "I recall the glass gate next to Gus in Lagos, near the gold bridge.";
-    deleteG(msg);
-    cout << msg;  // prints   I recall the lass ate next to us in Laos, near the old bride.
+int main () {
+    string *fp;
+    string fish[5];
+    fp = &fish[4];
+    *fp = "salmon";
+    *(fish + 3) = "yellowtail";
+    fp -= 3;
+    fp[1] = "eel";
+    fp[0] = "tuna";
+    bool d = fp == fish;
+    bool b = *fp == *(fp+1);
+    cout << b;
+    
+    for (int i = 0; i < 5; i++)
+        cout << fish[i] << "\n";
 }
